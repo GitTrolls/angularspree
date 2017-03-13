d9283a4869ba22ec2e34e83034fdd5abc0c9a9e4
GitTrolls <theaaronbyte@gmail.com>
2017-03-13 21:30:08 +0530

    unstable code for product preview

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
+import { ProductActions } from './../product/actions/product-actions';
+import { AppState } from './../interfaces';
+import { getProducts } from './../product/reducers/selectors';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../core/models/product';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products$: Observable<any>;
 
-  constructor() { }
+
+  constructor(private store: Store<AppState>, private actions: ProductActions) {
+    this.products$ = this.store.select(getProducts);
+
+    this.products$.subscribe(data => {
+      console.log('products ----->>>>>>>>', data);
+      window['t'] = data;
+    });
+  }
 
   ngOnInit() {
+      console.log('store object', this.store);
+      this.store.dispatch(this.actions.getAllProducts());
   }
 
 }
