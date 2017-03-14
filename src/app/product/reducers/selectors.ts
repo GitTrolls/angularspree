b56d5db33913cc4664f1505a04263ebc676e5343
GitTrolls <theaaronbyte@gmail.com>
2017-03-14 14:26:34 +0530

    updated ui auth

--- src/app/product/reducers/selectors.ts
+++ src/app/product/reducers/selectors.ts
@@ -4,27 +4,22 @@ import { ProductState } from './product-state';
 import { createSelector } from 'reselect';
 import { Map, List, fromJS } from 'immutable';
 
-// Base product state selector function
+// Base product state function
 export function getProductState(state: AppState): ProductState {
-  return state.products;
+    return state.products;
 }
 
 // ******************** Individual selectors ***************************
 export function fetchProducts(state: ProductState) {
-  const ids = state.productIds.toJS();
-  const productEntities = state.productEntities.toJS();
-  return ids.map(id => productEntities[id]);
+        const ids = state.productIds.toJS();
+        const productEntities = state.productEntities.toJS();
+        return ids.map(id => productEntities[id]);
 }
 
-export function fetchAllTaxonomies(state: ProductState) {
-  return state.taxonomies.toJS();
+const fetchSelectedProduct = function(state: ProductState): Product {
+    return state.selectedProduct;
 }
-
-const fetchSelectedProduct = function (state: ProductState): Product {
-  return state.selectedProduct;
-};
 
 // *************************** PUBLIC API's ****************************
 export const getSelectedProduct = createSelector(getProductState, fetchSelectedProduct);
 export const getProducts = createSelector(getProductState, fetchProducts);
-export const getTaxonomies = createSelector(getProductState, fetchAllTaxonomies);
