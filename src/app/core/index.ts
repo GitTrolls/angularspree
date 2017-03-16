be21ac3e1ff40e10e4a3d5190b8d6db88c3c537f
GitTrolls <theaaronbyte@gmail.com>
2017-03-16 17:44:59 +0530

    cart ui fixes

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,7 +1,6 @@
-import { CartService } from './services/cart.service';
+import { CheckoutService } from './services/checkout.service';
 import { NgModule } from '@angular/core';
 import { ProductDummyService } from './services/product-dummy.service';
-import { VariantParserService } from './services/variant-parser.service';
 import { HttpModule, XHRBackend, RequestOptions, Http } from '@angular/http';
 // Components
 
@@ -10,6 +9,7 @@ import { AuthService } from './services/auth.service';
 import { HttpService } from './services/http';
 import { ProductService } from './services/product.service';
 import { AuthActions } from '../auth/actions/auth.actions';
+
 
 export function httpInterceptor(
   backend: XHRBackend,
@@ -31,14 +31,13 @@ export function httpInterceptor(
   imports: [
   ],
   providers: [
-    VariantParserService,
     AuthService,
     {
       provide: HttpService,
       useFactory: httpInterceptor,
       deps: [ XHRBackend, RequestOptions]
     },
-    CartService,
+    CheckoutService,
     ProductDummyService,
     ProductService,
     AuthActions
